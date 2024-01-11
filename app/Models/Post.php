30d760c914b07a03a4af9849c7eb26745519d0fd
hirol413 <petuo1616@gmail.com>
2024-01-11 18:51:59 +0900

    投稿一覧修正

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,7 +32,7 @@ class Post extends Model
         {
             return $this->belongsTo(User::class);
         }
-        public function getPaginateByLimit(int $limit_count = 10)
+        public function getPaginateByLimit(int $limit_count = 5)
         {
             // updated_atで降順に並べたあと、limitで件数制限をかける
             return $this::with('user')->orderBy('updated_at', 'DESC')->paginate($limit_count);
